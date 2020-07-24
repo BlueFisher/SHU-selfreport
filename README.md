ccc55f955701a46b513e246b51338f71b89bee38
Fisher Chang <blue_fisher@qq.com>
2020-07-24 10:52:36 +0800

    增加配置文件，可以配置多个学号

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # 上海大学每日一报自动打卡
 
-修改自己的学号与密码，挂在服务器上即可，对于在校同学早上7点晚上8点左右会自动打卡
+在配置文件 `config.yaml` 中修改学号密码，挂在服务器上即可。
+
+对于在校同学早上7点晚上20点10-11分左右会自动打卡。由于服务器数据库有一定概率出问题，实际提交成功，但服务器返回的是失败信息，如果配置了邮件服务器的话会在失败的情况下发邮件提醒（成功时不发）。
+
+程序运行中修改 `config.yaml` 可以在下一次提交时生效。
+
+邮件服务器不是必须的，根据需要修改。
 
 ## 用法：
+1. 修改 `config.yaml`
+
+2. 启动程序
 ```shell
-nohup python -u main.py -i 10000000 -p SHUv587 -f 12345@qq.com -e qqmima -t 54321@qq.com > shu_report.log 2>&1 &
+nohup python -u main.py > shu_report.log 2>&1 &
+或指定配置文件位置
+nohup python -u main.py -c config.yaml > shu_report.log 2>&1 &
 ```
 
--- main.py
+++ main.py
@@ -5,86 +5,101 @@ import requests
 import datetime as dt
 
 from bs4 import BeautifulSoup
+import yaml
 from email.message import EmailMessage
 
 
-def send_mail(to_email, subject, message, username, password):
+config_file_path = 'config.yaml'
+
+
+def send_mail(to_email, subject, message):
+    with open(config_file_path) as f:
+        config_file = yaml.load(f, Loader=yaml.FullLoader)
+
     msg = EmailMessage()
     msg['Subject'] = subject
-    msg['From'] = "xxxx@qq.com"
+    msg['From'] = config_file['email']['from']
     msg['To'] = ', '.join(to_email)
     msg.set_content(message)
-    server = smtplib.SMTP('smtp.qq.com')
-    server.login(username, password)
+    server = smtplib.SMTP(config_file['email']['smtp'])
+    server.login(config_file['email']['username'], config_file['email']['password'])
     server.send_message(msg)
     server.quit()
 
+
+def report(username, password, email, ii):
+    print('report', username)
+    sess = requests.Session()
+    sess.post("https://newsso.shu.edu.cn/login", data={
+        'username': username,
+        'password': password,
+        'login_submit': '%25E7%2599%25BB%25E5%25BD%2595%252FLogin'
+    })
+    sess.get('https://newsso.shu.edu.cn/oauth/authorize?response_type=code&client_id=WUHWfrntnWYHZfzQ5QvXUCVy&redirect_uri=https%3a%2f%2fselfreport.shu.edu.cn%2fLoginSSO.aspx%3fReturnUrl%3d%252fDefault.aspx&scope=1')
+
+    url = 'https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?t=' + ii
+    r = sess.get(url)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    r = sess.post(url, data={
+        '__EVENTTARGET': 'p1$ctl00$btnSubmit',
+        '__VIEWSTATE': soup.find('input', attrs={'name': '__VIEWSTATE'})['value'],
+        '__VIEWSTATEGENERATOR': 'DC4D08A3',
+        'p1$ChengNuo': 'p1_ChengNuo',
+        'p1$BaoSRQ': t.strftime('%Y-%m-%d'),
+        'p1$DangQSTZK': '良好',
+        'p1$TiWen': '37',
+        'p1$SuiSM': '绿色',
+        'p1$ShiFJC': ['早餐', '午餐', '晚餐'],
+        'F_TARGET': 'p1_ctl00_btnSubmit',
+        'p1_Collapsed': 'false',
+    }, headers={
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-FineUI-Ajax': 'true'
+    }, allow_redirects=False)
+
+    if '提交成功' in r.text:
+        print(t.strftime('%Y-%m-%d %H:%M:%S'), '提交成功')
+        return
+
+    if email is not None:
+        send_mail(to_email=[email],
+                  subject=t.strftime('%Y-%m-%d %H:%M:%S') + '提交不确定，请手动查看',
+                  message=t.strftime('%Y-%m-%d %H:%M:%S') + '提交不确定，请手动查看')
+    print(t.strftime('%Y-%m-%d %H:%M:%S'), '提交不确定，请手动查看')
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    # 登录，学号密码换成自己的
-    parser.add_argument('--id', '-i', type=str, default='xxxxxxxx')
-    parser.add_argument('--pwd', '-p', type=str, default='xxxxxxxx')
-    # 发送邮件的邮箱
-    parser.add_argument('--from-email', '-f', type=str, default='xxxx@qq.com')
-    parser.add_argument('--email-pwd', '-e', type=str, default='xxxxxxxx')
-    # 接受邮件的邮箱
-    parser.add_argument('--to-email', '-t', type=str, default='xxxx@qq.com')
+    # 配置文件
+    parser.add_argument('--config', '-c', type=str, default='config.yaml' help='配置文件位置')
     args = parser.parse_args()
+    config_file_path = args.config
 
-    username = args.id
-    passward = args.pwd
-    from_email = args.from_email
-    email_pwd = args.email_pwd
-    to_email = args.to_email
+    # 立即提交一次
+    # 服务器上的时区是0时区，TODO 怎么在python里换时区？不想查了
+    t = dt.datetime.utcnow()
+    t = t + dt.timedelta(hours=8)
+
+    ii = '1' if t.hour < 20 else '2'
+    with open(config_file_path) as f:
+        config_file = yaml.load(f, Loader=yaml.FullLoader)
+        for user in config_file['users']:
+            report(user['id'], user['pwd'], user['email_to'], ii)
 
     while True:
-        # 服务器上的时区是0时区，TODO 怎么在python里换时区？不想查了
         t = dt.datetime.utcnow()
         t = t + dt.timedelta(hours=8)
 
-        # 早上7点与晚上20点2分、3分左右打卡
+        # 早上7点与晚上20点10分、11分左右打卡
         if t.hour == 7 or t.hour == 20:
-            if t.minute in [2, 3]:
+            if t.minute in [10, 11]:
                 ii = '1' if t.hour == 7 else '2'
+                with open(config_file_path) as f:
+                    config_file = yaml.load(f, Loader=yaml.FullLoader)
+                    for user in config_file['users']:
+                        report(user['id'], user['pwd'], user['email_to'], ii)
 
-                sess = requests.Session()
-                sess.post("https://newsso.shu.edu.cn/login", data={
-                    'username': username,
-                    'password': passward,
-                    'login_submit': '%25E7%2599%25BB%25E5%25BD%2595%252FLogin'
-                })
-                sess.get('https://newsso.shu.edu.cn/oauth/authorize?response_type=code&client_id=WUHWfrntnWYHZfzQ5QvXUCVy&redirect_uri=https%3a%2f%2fselfreport.shu.edu.cn%2fLoginSSO.aspx%3fReturnUrl%3d%252fDefault.aspx&scope=1')
-
-                # 打卡，这里是在校学生早晚打卡，没有返校的应该不是这个API
-                url = 'https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?t=' + ii
-                r = sess.get(url)
-                soup = BeautifulSoup(r.text, 'html.parser')
-                r = sess.post(url, data={
-                    '__EVENTTARGET': 'p1$ctl00$btnSubmit',
-                    '__VIEWSTATE': soup.find('input', attrs={'name': '__VIEWSTATE'})['value'],
-                    '__VIEWSTATEGENERATOR': 'DC4D08A3',
-                    'p1$ChengNuo': 'p1_ChengNuo',
-                    'p1$BaoSRQ': t.strftime('%Y-%m-%d'),
-                    'p1$DangQSTZK': '良好',
-                    'p1$TiWen': '37',
-                    'p1$SuiSM': '绿色',
-                    'p1$ShiFJC': ['早餐', '午餐', '晚餐'],
-                    'F_TARGET': 'p1_ctl00_btnSubmit',
-                    'p1_Collapsed': 'false',
-                }, headers={
-                    'X-Requested-With': 'XMLHttpRequest',
-                    'X-FineUI-Ajax': 'true'
-                }, allow_redirects=False)
-
-                if '提交成功' in r.text:
-                    # 可以发送邮件，以免发生错误，不需要的话删除即可
-                    send_mail(to_email=[to_email],
-                            subject=t.strftime('%Y-%m-%d %H:%M:%S') + '提交成功',
-                            message=t.strftime('%Y-%m-%d %H:%M:%S') + '提交成功',
-                            username=from_email,
-                            password=email_pwd)
-                    print(t.strftime('%Y-%m-%d %H:%M:%S'), '提交成功')
-
-                    time.sleep(60)
+                time.sleep(60)
 
         time.sleep(60)
