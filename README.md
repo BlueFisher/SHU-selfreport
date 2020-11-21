ef803c341adfd3f17062b9d1a78a6b616ff2ad37
Fisher Chang <blue_fisher@qq.com>
2020-11-21 20:08:39 +0800

    加入一键自动补报功能，精简代码

--- README.md
+++ README.md
@@ -2,87 +2,28 @@
 
 程序为python脚本文件，修改配置文件相关信息，设置后台运行脚本，脚本会根据配置文件信息自动进行每日两报。
 
-## 特点
-
-- 可在`setting_config.yaml`自定义打卡时间、报送体温。
-
-- 如果配置了邮件服务器的话会在填报失败的情况下发邮件提醒。
-
-  设置原因：由于学校每日两报服务器数据库有一定概率出问题，实际提交成功，但服务器返回的是失败信息。
-
-- 可在`setting_config.yaml`自定义填报成功是否进行邮件提醒。
-
-- 程序运行中修改 `report_config.yaml` 可以在下一次提交时生效。
-
-- 邮件服务器不是必须的，根据需要修改。
-
-- 邮件smtp服务器更改为使用SSL协议端口号，可以适应阿里云服务器等默认关闭25端口的服务器。
+此版本为精简代码，并加入了一键补报功能。
 
 ## 用法
-1. 修改 `report_config.yaml`
 
-   ```yaml
-   # 邮件提醒服务设置，如果不需要， 将email下的字段设置为null即可
-   email:
-     from: "12345@qq.com" # 发送邮件的地址
-     username: "12345@qq.com" # 登陆邮箱的账号
-     password: "QQmima" # smtp服务授权码，请进入邮箱设置进行查看
-     smtp: "smtp.qq.com" # smtp服务器
-     port: 465 # smtp服务器SSL协议端口号，大多数邮箱默认为465
-   
-   users:
-     - id: "user1" # 学号
-       pwd: "pwd"	# 密码
-       email_to: "user1@qq.com"	# 接受报送成功邮件的邮箱地址
-     - id: "user2"
-       pwd: "pwd"
-       email_to: "user2@qq.com"
-     - id: "user3"
-       pwd: "pwd"
-       email_to: "user3@qq.com"
-   ```
-   
-2. 修改`setting_config.yaml`
+在 `config.yaml` 中设置所有需要打卡的学号密码
 
-   ```yaml
-   # 设置“每日一报”填报成功是否发送邮件
-   # true：发送
-   # false：不发送
-   send_email: false
-   
-   # 早上申报时间,建议填写7点或8点，依据自己需要填写
-   morning_hour: 7
-   
-   # 如morning_hour和night_hour填写为7和20，minute填为10，则会在7:10-7:20、20:10-20:20进行填报，建议填0~50之间的分钟数
-   minute: 10
-   
-   # 温度
-   # 如填写36.3，则会在36.3-0.2， 36.3+0.2之间选择随机数填报
-   temperature: 36.3
-   ```
+**本程序自带一键补报功能**，如需补报，定位到 `main.py` 第136行
 
-3. 测试（可选）
-
-   - 测试填写的所有账号密码是否正确
-
-     ```python
-     python main.py -r
-     ```
-
-   - 测试邮箱服务是否可使用
-
-     ```python
-     python main.py -e -a xxx@xx.com # xxx@xx.com为接收测试邮件的账号
-     ```
+```python
+# 如果需要补报，则将 `args` 第三个参数改为 `True`，第四个参数为补报的月份区间，默认是10到11月
+# 如果不需要补报，则将 `args` 第三个参数改为 `False`
+t = threading.Thread(target=report_threading, args=(sess, user['id'], True, range(10, 12)), daemon=True)
+```
 
 4. 启动程序：
 
-   ```python
-   nohup python main.py > shu_report.log 2>&1 & # 针对Linux，启动程序，后台运行，输出结果导出shu_report.log
-   # 如需设置开机自启动，请自行搜索网上教程
-   ```
+```bash
+# 针对Linux，启动程序，后台运行，输出结果导出shu_report.log
+nohup python -u main.py > shu_report.log 2>&1 &
+```
 
-## 要求
+## 依赖
 
 - python3
 - 依赖：
--- main.py
+++ main.py
@@ -1,28 +1,142 @@
-import argparse
+import datetime as dt
+import time
+import threading
 
-from utils import test_report, test_send_email, auto_report
+import requests
+import yaml
+from bs4 import BeautifulSoup
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--test_report', '-r', action='store_true', help='测试账号是否正确')
-    parser.add_argument('--test_send_email', '-e', action='store_true', help='测试邮件发送模块')
-    parser.add_argument("--account", '-a', type=str, help="选择了测试邮件功能，使用该选项输入测试邮件收件邮箱")
-    args = parser.parse_args()
 
-    setting_config_path = 'setting_config.yaml'
-    report_config_path = 'report_config.yaml'
+# 获取东八区时间
+def get_time():
+    # 获取0时区时间，变换为东八区时间
+    # 原因：运行程序的服务器所处时区不确定
+    t = dt.datetime.utcnow()
+    t = t + dt.timedelta(hours=8)
 
-    if args.test_report:
-        test_report(report_config_path)
+    # 或者：
+    # t = dt.datetime.utcnow()
+    # tz_utc_8 = dt.timezone(dt.timedelta(hours=8))
+    # t = t.astimezone(tz_utc_8)
 
-    if args.test_send_email:
-        if args.account is None:
-            print("邮件发送功能错误：请使用参数'--account'或'-a'输入收件邮箱")
-        else:
-            test_send_email(args.account, report_config_path)
+    # 如果服务器位于东八区，也可用：
+    # t = dt.datetime.now()
 
-    if args.test_report or args.test_send_email:
-        exit(0)
+    return t
 
-    auto_report(report_config_path, setting_config_path)
+
+def login(username, password):
+    sess = requests.Session()
+    while True:
+        try:
+            r = sess.get('https://selfreport.shu.edu.cn/Default.aspx')
+            sess.post(r.url, data={
+                'username': username,
+                'password': password
+            })
+            sess.get('https://newsso.shu.edu.cn/oauth/authorize?response_type=code&client_id=WUHWfrntnWYHZfzQ5QvXUCVy&redirect_uri=https%3a%2f%2fselfreport.shu.edu.cn%2fLoginSSO.aspx%3fReturnUrl%3d%252fDefault.aspx&scope=1')
+        except Exception as e:
+            print(e)
+            continue
+        break
+
+    url = f'https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?day=2020-11-21&t=1'
+    while True:
+        try:
+            r = sess.get(url)
+        except Exception as e:
+            print(e)
+            continue
+        break
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    view_state = soup.find('input', attrs={'name': '__VIEWSTATE'})
+
+    if view_state is None:
+        print(f'{username} 登录失败')
+        print(r.text)
+        return
+
+    print(f'{username} 登录成功')
+
+    return sess
+
+
+def report(sess, username, t, temperature=37):
+    ii = '1' if t.hour < 20 else '2'
+    url = f'https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?day={t.year}-{t.month}-{t.day}&t={ii}'
+    while True:
+        try:
+            r = sess.get(url)
+        except Exception as e:
+            print(e)
+            continue
+        break
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    view_state = soup.find('input', attrs={'name': '__VIEWSTATE'})
+
+    if view_state is None:
+        print(r.text)
+        return False
+
+    r = sess.post(url, data={
+        '__EVENTTARGET': 'p1$ctl00$btnSubmit',
+        '__VIEWSTATE': view_state['value'],
+        '__VIEWSTATEGENERATOR': 'DC4D08A3',
+        'p1$ChengNuo': 'p1_ChengNuo',
+        'p1$BaoSRQ': t.strftime('%Y-%m-%d'),
+        'p1$DangQSTZK': '良好',
+        'p1$TiWen': str(temperature),
+        'p1$SuiSM': '绿色',
+        'p1$ShiFJC': ['早餐', '午餐', '晚餐'],
+        'F_TARGET': 'p1_ctl00_btnSubmit',
+        'p1_Collapsed': 'false',
+    }, headers={
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-FineUI-Ajax': 'true'
+    }, allow_redirects=False)
+
+    if '提交成功' or '历史信息不能修改' in r.text:
+        print(f'{username} {t} 提交成功')
+    else:
+        print(r.text)
+
+
+with open('config.yaml', encoding='utf8') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+
+def report_threading(sess, username, need_before=False, months=None):
+    if need_before:
+        for month in months:
+            for day in range(1, 32):
+                for hour in [9, 21]:
+                    try:
+                        t = dt.datetime(2020, month, day, hour)
+                    except ValueError:
+                        continue
+
+                    report(sess, username, t)
+
+    while True:
+        t = get_time()
+        report(sess, username, t)
+        # 每个小时填报一次
+        time.sleep(60 * 60)
+
+
+for user in config['users']:
+    sess = login(user['id'], user['pwd'])
+    if sess:
+        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # 如果需要补报，则将 `args` 第三个参数改为 `True`，第四个参数为补报的月份区间，默认是10到11月
+        # 如果不需要补报，则将 `args` 第三个参数改为 `False`
+        t = threading.Thread(target=report_threading, args=(sess, user['id'], True, range(10, 12)), daemon=True)
+        t.start()
+
+    # 每次登录间隔60秒，否则登陆系统会报错
+    time.sleep(60)
+
+t.join()
