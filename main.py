d2a5aed298af980c18a37e0319c66caf55ffb60b
Fisher Chang <blue_fisher@qq.com>
2020-11-30 10:33:41 +0800

    重大更新

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import time
 
 import requests
 import yaml
+import argparse
+import json
 from bs4 import BeautifulSoup
 from fState import F_STATE_GENERATOR
 
 NEED_BEFORE = True  # 如需补报则置为True，否则False
 MONTHS = [10, 11]  # 补报的月份，默认10月、11月
-ZAIXIAO = "宝山"  # 宝山、嘉定或延长
-XIAN = "宝山区"  # 宝山区、嘉定区或静安区
+XIAOQU = "宝山"  # 宝山、嘉定或延长
 
 
 # 获取东八区时间
@@ -67,8 +68,15 @@ def login(username, password):
     return sess
 
 
-def report(sess, t, temperature=37):
+def report(sess, t, xiaoqu='宝山', temperature=37):
     ii = '1' if t.hour < 20 else '2'
+    if xiaoqu == '宝山':
+        xian = '宝山区'
+    elif xiaoqu == '嘉定':
+        xian = '嘉定区'
+    elif xiaoqu == '延长':
+        xian = '经验区'
+
     url = f'https://selfreport.shu.edu.cn/XueSFX/HalfdayReport.aspx?day={t.year}-{t.month}-{t.day}&t={ii}'
     while True:
         try:
@@ -85,50 +93,56 @@ def report(sess, t, temperature=37):
         print(r.text)
         return False
 
-    r = sess.post(url, data={
-        '__EVENTTARGET': 'p1$ctl00$btnSubmit',
-        '__VIEWSTATE': view_state['value'],
-        '__VIEWSTATEGENERATOR': 'DC4D08A3',
-        'p1$ChengNuo': 'p1_ChengNuo',
-        'p1$BaoSRQ': t.strftime('%Y-%m-%d'),
-        'p1$DangQSTZK': '良好',
-        'p1$TiWen': str(temperature),
-        'p1$TiWen': '37',
-        'p1$ZaiXiao': ZAIXIAO,
-        'p1$ddlSheng$Value': '上海',
-        'p1$ddlSheng': '上海',
-        'p1$ddlShi$Value': '上海市',
-        'p1$ddlShi': '上海市',
-        'p1$ddlXian$Value': XIAN,
-        'p1$ddlXian': XIAN,
-        'p1$FengXDQDL': '否',
-        'p1$TongZWDLH': '否',
-        'p1$XiangXDZ': '上海大学',
-        'p1$QueZHZJC$Value': '否',
-        'p1$QueZHZJC': '否',
-        'p1$DangRGL': '否',
-        'p1$GeLDZ': '',
-        'p1$CengFWH': '否',
-        'p1$CengFWH_RiQi': '',
-        'p1$CengFWH_BeiZhu': '',
-        'p1$JieChu': '否',
-        'p1$JieChu_RiQi': '',
-        'p1$JieChu_BeiZhu': '',
-        'p1$TuJWH': '否',
-        'p1$TuJWH_RiQi': '',
-        'p1$TuJWH_BeiZhu': '',
-        'p1$JiaRen_BeiZhu': '',
-        'p1$SuiSM': '绿色',
-        'p1$LvMa14Days': '是',
-        'p1$Address2': '',
-        'p1_GeLSM_Collapsed': 'false',
-        'p1_Collapsed': 'false',
-        'F_TARGET': 'p1_ctl00_btnSubmit',
-        'F_STATE': F_STATE_GENERATOR().updated_F_STATE(t, ZAIXIAO, XIAN, ii),
-    }, headers={
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-FineUI-Ajax': 'true'
-    }, allow_redirects=False)
+    while True:
+        try:
+            r = sess.post(url, data={
+                '__EVENTTARGET': 'p1$ctl00$btnSubmit',
+                '__VIEWSTATE': view_state['value'],
+                '__VIEWSTATEGENERATOR': 'DC4D08A3',
+                'p1$ChengNuo': 'p1_ChengNuo',
+                'p1$BaoSRQ': t.strftime('%Y-%m-%d'),
+                'p1$DangQSTZK': '良好',
+                'p1$TiWen': str(temperature),
+                'p1$TiWen': '37',
+                'p1$ZaiXiao': xiaoqu,
+                'p1$ddlSheng$Value': '上海',
+                'p1$ddlSheng': '上海',
+                'p1$ddlShi$Value': '上海市',
+                'p1$ddlShi': '上海市',
+                'p1$ddlXian$Value': xian,
+                'p1$ddlXian': xian,
+                'p1$FengXDQDL': '否',
+                'p1$TongZWDLH': '否',
+                'p1$XiangXDZ': '上海大学',
+                'p1$QueZHZJC$Value': '否',
+                'p1$QueZHZJC': '否',
+                'p1$DangRGL': '否',
+                'p1$GeLDZ': '',
+                'p1$CengFWH': '否',
+                'p1$CengFWH_RiQi': '',
+                'p1$CengFWH_BeiZhu': '',
+                'p1$JieChu': '否',
+                'p1$JieChu_RiQi': '',
+                'p1$JieChu_BeiZhu': '',
+                'p1$TuJWH': '否',
+                'p1$TuJWH_RiQi': '',
+                'p1$TuJWH_BeiZhu': '',
+                'p1$JiaRen_BeiZhu': '',
+                'p1$SuiSM': '绿色',
+                'p1$LvMa14Days': '是',
+                'p1$Address2': '',
+                'p1_GeLSM_Collapsed': 'false',
+                'p1_Collapsed': 'false',
+                'F_TARGET': 'p1_ctl00_btnSubmit',
+                'F_STATE': F_STATE_GENERATOR().updated_F_STATE(t, xiaoqu, xian, ii),
+            }, headers={
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-FineUI-Ajax': 'true'
+            }, allow_redirects=False)
+        except Exception as e:
+            print(e)
+            continue
+        break
 
     if any(i in r.text for i in ['提交成功', '历史信息不能修改', '现在还没到晚报时间', '只能填报当天或补填以前的信息']):
         print(f'{t} 提交成功')
@@ -138,41 +152,128 @@ def report(sess, t, temperature=37):
         return False
 
 
+def request_for_auto_report(username, sess):
+    print(username)
+    cookie = dict()
+    for k, v in sess.cookies.items():
+        cookie[k] = v
+
+    token = json.dumps(cookie)
+    print(token)
+
+    r = requests.post('http://shu-report.shusnjl.cn/update', json={
+        'id': username,
+        'campus': XIAOQU,
+        'token': token
+    })
+
+    if r.status_code == 200:
+        if r.json()['msg'] == 'insert success':
+            print('已提交到自动填报服务器，如果支持我们，请记得捐赠呦')
+        elif r.json()['msg'] == 'update success':
+            print('已更新到自动填报服务器，如果支持我们，请记得捐赠呦')
+        else:
+            print(r.json())
+    else:
+        print('提交失败')
+        print(r.text)
+
+
+def request_for_saved_sess():
+    r = requests.get('http://shu-report.shusnjl.cn/token')
+    if r.status_code == 200:
+        res = r.json()
+        token = res['token']
+        try:
+            cookie = json.loads(token)
+        except:
+            print(f'token错误 {token}')
+            return
+        sess = requests.Session()
+        for k, v in cookie.items():
+            sess.cookies.set(k, v)
+        return res['id'], res['campus'], sess
+
+
+def send_report_result(username, status):
+    r = requests.post('http://shu-report.shusnjl.cn/status', json={
+        'id': username,
+        'status': status
+    })
+    if r.status_code == 200:
+        print('已提交结果')
+
+
 with open('config.yaml', encoding='utf8') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
-last_login_time = 0
-user_login_status = {user: {'sess': None, 'has_before': False} for user in config}
 
-while True:
+parser = argparse.ArgumentParser()
+parser.add_argument('--request', action='store_true')
+parser.add_argument('--server', action='store_true')
+
+args = parser.parse_args()
+
+
+if args.request and args.server:
+    print("参数错误")
+    exit()
+
+if args.request:
     for user in config:
         print(f'======{user}======')
-        if user_login_status[user]['sess'] is None:
-            if time.time() - last_login_time > 60:
-                user_login_status[user]['sess'] = login(user, config[user]['pwd'])
-                last_login_time = time.time()
-            else:
-                print('等待登录')
-
-        sess = user_login_status[user]['sess']
+        sess = login(user, config[user]['pwd'])
         if sess:
-            if NEED_BEFORE and not user_login_status[user]['has_before']:
-                for month in MONTHS:
-                    for day in range(1, 32):
-                        for hour in [9, 21]:
-                            try:
-                                t = dt.datetime(2020, month, day, hour)
-                            except ValueError:
-                                continue
-
-                            if not report(sess, t):
-                                user_login_status[user]['sess'] = None
-                                user_login_status[user]['has_before'] = False
-                                break
-                            else:
-                                user_login_status[user]['has_before'] = True
-
+            request_for_auto_report(user, sess)
+        print(f'等待一分钟')
+        time.sleep(60)
+elif args.server:
+    while True:
+        response = request_for_saved_sess()
+        if response:
+            username, xiaoqu, sess = response
             t = get_time()
-            if not report(sess, t):
-                user_login_status[user]['sess'] = None
+            result = report(sess, t, xiaoqu)
+            send_report_result(username, result)
+            print(f'已填报 {username}')
+        else:
+            print('没有需要填报的数据')
 
-    time.sleep(60 * 10)
+        time.sleep(60 * 5)
+else:
+    last_login_time = 0
+    user_login_status = {user: {'sess': None, 'has_before': False} for user in config}
+
+    while True:
+        for user in config:
+            print(f'======{user}======')
+            if user_login_status[user]['sess'] is None:
+                if time.time() - last_login_time > 60:
+                    user_login_status[user]['sess'] = login(user, config[user]['pwd'])
+                    last_login_time = time.time()
+                else:
+                    print('等待登录')
+
+            sess = user_login_status[user]['sess']
+            if sess:
+                if NEED_BEFORE and not user_login_status[user]['has_before']:
+                    for month in MONTHS:
+                        for day in range(1, 32):
+                            for hour in [9, 21]:
+                                try:
+                                    t = dt.datetime(2020, month, day, hour)
+                                except ValueError:
+                                    continue
+
+                                if not report(sess, t, XIAOQU):
+                                    user_login_status[user]['sess'] = None
+                                    user_login_status[user]['has_before'] = False
+                                    break
+                                else:
+                                    user_login_status[user]['has_before'] = True
+
+                t = get_time()
+                if not report(sess, t, XIAOQU):
+                    user_login_status[user]['sess'] = None
+
+        time.sleep(60 * 10)
+        
