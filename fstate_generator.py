60df0f6c83fea5f12eb4be76a1e3ce8651ce0ac6
Fisher Chang <blue_fisher@qq.com>
2021-01-29 12:06:39 +0800

    支持用前一天信息填报

--- fstate_generator.py
+++ fstate_generator.py
@@ -1,6 +1,8 @@
 import base64
+import datetime as dt
 import json
-from os import fstat
+import random
+import re
 from pathlib import Path
 
 
@@ -10,11 +12,19 @@ def _generate_fstate_base64(fstate):
     return base64.b64encode(fstate_bytes).decode()
 
 
-def generate_fstate_day(BaoSRQ, XiangXDZ):
-    with open(Path(__file__).resolve().parent.joinpath('fstate_day.json'), encoding='utf8') as f:
+def generate_fstate_day(BaoSRQ, ShiFSH, ShiFZX, ddlSheng, ddlShi, ddlXian, XiangXDZ):
+    with open('fstate_day.json', encoding='utf8') as f:
         fstate = json.loads(f.read())
 
     fstate['p1_BaoSRQ']['Text'] = BaoSRQ
+    fstate['p1_ShiFSH']['SelectedValue'] = ShiFSH
+    fstate['p1_ShiFZX']['SelectedValue'] = ShiFZX
+    fstate['p1_ddlSheng']['F_Items'] = [[ddlSheng, ddlSheng, 1, '', '']]
+    fstate['p1_ddlSheng']['SelectedValueArray'] = [ddlSheng]
+    fstate['p1_ddlShi']['F_Items'] = [[ddlShi, ddlShi, 1, '', '']]
+    fstate['p1_ddlShi']['SelectedValueArray'] = [ddlShi]
+    fstate['p1_ddlXian']['F_Items'] = [[ddlXian, ddlXian, 1, '', '']]
+    fstate['p1_ddlXian']['SelectedValueArray'] = [ddlXian]
     fstate['p1_XiangXDZ']['Text'] = XiangXDZ
 
     fstate_base64 = _generate_fstate_base64(fstate)
@@ -33,6 +43,48 @@ def generate_fstate_halfday(BaoSRQ):
     fstate_base64 = _generate_fstate_base64(fstate)
 
     return fstate_base64
+
+
+def _html_to_json(html):
+    return json.loads(html[html.find('=') + 1:])
+
+
+# 随机生成地址
+def get_random_address():
+    address = [chr(random.randint(0x4e00, 0x9fbf)) for _ in range(3)]
+    address = ''.join(address) + '路'
+    address += str(random.randint(1, 999)) + '号'
+    address += str(random.randint(1, 20)) + '0' + str(random.randint(1, 9)) + '室'
+    return address
+
+
+def get_last_report(sess, t):
+    ShiFSH = '是'
+    ShiFZX = '否'
+    ddlSheng = '上海'
+    ddlShi = '上海市'
+    ddlXian = '宝山区'
+    XiangXDZ = get_random_address()
+
+    t = t - dt.timedelta(days=1)
+    r = sess.get(f'https://selfreport.shu.edu.cn/ViewDayReport.aspx?day={t.year}-{t.month}-{t.day}')
+    t = re.findall(r'^.*//\]', r.text, re.MULTILINE)[0]
+    htmls = t.split(';var ')
+    for i, h in enumerate(htmls):
+        if 'p1$ShiFSH' in h:
+            ShiFSH = _html_to_json(htmls[i - 1])['SelectedValue']
+        if '"p1$ShiFZX' in h:
+            ShiFZX = _html_to_json(htmls[i - 1])['SelectedValue']
+        if 'p1$ddlSheng' in h:
+            ddlSheng = _html_to_json(htmls[i - 1])['SelectedValueArray'][0]
+        if 'p1$ddlShi' in h:
+            ddlShi = _html_to_json(htmls[i - 1])['SelectedValueArray'][0]
+        if 'p1$ddlXian' in h:
+            ddlXian = _html_to_json(htmls[i - 1])['SelectedValueArray'][0]
+        if 'p1$XiangXDZ' in h:
+            XiangXDZ = _html_to_json(htmls[i - 1])['Text']
+
+    return ShiFSH, ShiFZX, ddlSheng, ddlShi, ddlXian, XiangXDZ
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import datetime as dt
 import os
-import random
 import time
 from pathlib import Path
 
-import requests
 import yaml
 from bs4 import BeautifulSoup
 
-from fstate_generator import generate_fstate_day, generate_fstate_halfday
+from fstate_generator import generate_fstate_day, generate_fstate_halfday, get_last_report
 from login import login
 
 NEED_BEFORE = False  # 如需补报则置为True，否则False
@@ -33,15 +31,6 @@ def get_time():
     return t
 
 
-# 随机生成地址
-def get_random_address():
-    address = [chr(random.randint(0x4e00, 0x9fbf)) for _ in range(3)]
-    address = ''.join(address) + '路'
-    address += str(random.randint(1, 999)) + '号'
-    address += str(random.randint(1, 20)) + '0' + str(random.randint(1, 9)) + '室'
-    return address
-
-
 def report_day(sess, t):
     url = f'https://selfreport.shu.edu.cn/DayReport.aspx?day={t.year}-{t.month}-{t.day}'
 
@@ -57,7 +46,8 @@ def report_day(sess, t):
         return False
 
     BaoSRQ = t.strftime('%Y-%m-%d')
-    XiangXDZ = get_random_address()
+    ShiFSH, ShiFZX, ddlSheng, ddlShi, ddlXian, XiangXDZ = get_last_report(sess, t)
+    print(f'是否在上海：{ShiFSH}', f'是否在校：{ShiFZX}', ddlSheng, ddlShi, ddlXian, XiangXDZ)
 
     while True:
         try:
@@ -86,14 +76,14 @@ def report_day(sess, t):
                 "p1$GuoNei": "国内",
                 "p1$ddlGuoJia$Value": "-1",
                 "p1$ddlGuoJia": "选择国家",
-                "p1$ShiFSH": "是",
-                "p1$ShiFZX": "否",
-                "p1$ddlSheng$Value": "上海",
-                "p1$ddlSheng": "上海",
-                "p1$ddlShi$Value": "上海市",
-                "p1$ddlShi": "上海市",
-                "p1$ddlXian$Value": "宝山区",
-                "p1$ddlXian": "宝山区",
+                "p1$ShiFSH": ShiFSH,
+                "p1$ShiFZX": ShiFZX,
+                "p1$ddlSheng$Value": ddlSheng,
+                "p1$ddlSheng": ddlSheng,
+                "p1$ddlShi$Value": ddlShi,
+                "p1$ddlShi": ddlShi,
+                "p1$ddlXian$Value": ddlXian,
+                "p1$ddlXian": ddlXian,
                 "p1$XiangXDZ": XiangXDZ,
                 "p1$FengXDQDL": "否",
                 "p1$TongZWDLH": "否",
@@ -122,7 +112,8 @@ def report_day(sess, t):
                 "p1_ContentPanel1_Collapsed": "true",
                 "p1_GeLSM_Collapsed": "false",
                 "p1_Collapsed": "false",
-                "F_STATE": generate_fstate_day(BaoSRQ, XiangXDZ)
+                "F_STATE": generate_fstate_day(BaoSRQ, ShiFSH, ShiFZX,
+                                               ddlSheng, ddlShi, ddlXian, XiangXDZ)
             }, headers={
                 'X-Requested-With': 'XMLHttpRequest',
                 'X-FineUI-Ajax': 'true'
