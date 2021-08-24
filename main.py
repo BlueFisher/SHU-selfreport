2bdbc6698a267ad98ed9bef1f72499f9196d6baf
zhangzhousuper <52350596+zhangzhousuper@users.noreply.github.com>
2021-08-24 17:49:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from bs4 import BeautifulSoup
 from fstate_generator import generate_fstate_day, generate_fstate_halfday, get_last_report
 from login import login
 
-NEED_BEFORE = False  # 如需补报则置为True，否则False
-START_DT = dt.datetime(2021, 4, 20)  # 需要补报的起始日期
+NEED_BEFORE = True  # 如需补报则置为True，否则False
+START_DT = dt.datetime(2021, 7, 26)  # 需要补报的起始日期
 RETRY = 5
 RETRY_TIMEOUT = 120
 
