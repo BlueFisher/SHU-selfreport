7c6a5cc684789f799194f13995b01e27e0d23153
Fisher Chang <blue_fisher@qq.com>
2022-03-05 12:45:56 +0800

    识别健康E档案动态采集，请手动填报

--- login.py
+++ login.py
@@ -1,6 +1,8 @@
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 RETRY = 5
 RETRY_TIMEOUT = 120
@@ -12,6 +14,9 @@ def login(browser, username, password):
 
         try:
             browser.get('https://selfreport.shu.edu.cn/Default.aspx')
+            WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.ID, 'username'))
+            )
             browser.find_element(By.ID, 'username').send_keys(username)
             browser.find_element(By.ID, 'password').send_keys(password)
             browser.find_element(By.ID, 'submit-button').click()
@@ -22,6 +27,13 @@ def login(browser, username, password):
         time.sleep(1)
         if browser.current_url == 'https://selfreport.shu.edu.cn/DayReport.aspx':
             return True
+        else:
+            if 'HSJC' in browser.current_url:
+                print('请立即手动填报核酸检测信息')
+                print(browser.current_url)
+                return False
+
+            print(browser.current_url)
 
         time.sleep(RETRY_TIMEOUT)
 
