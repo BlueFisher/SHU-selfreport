86fa9c2397b92cb1daf056e813ed60dfe085ce5a
Fisher Chang <blue_fisher@qq.com>
2022-08-22 10:55:09 +0800

    适配风险区

--- main.py
+++ main.py
@@ -120,6 +120,7 @@ def report_day(sess, t):
                 "p1$XiangXDZ": XiangXDZ,
                 "p1$ShiFZJ": ShiFZJ,
                 "p1$QueZHZJC$Value": "否",
+                "p1$GaoZDFXLJS": "无",
                 "p1$QueZHZJC": "否",
                 "p1$DangRGL": "否",
                 "p1$GeLDZ": "",
